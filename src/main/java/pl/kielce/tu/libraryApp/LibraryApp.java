518f68875546a2ccf8c48dd8e88c3d6fdff8ffb6
ciepluchs <Szymon.Ciepluch_ext@hoefliger.de>
2020-05-01 20:59:33 +0200

    Initial commit - gradle, main with hello world

---
+++ src/main/java/pl/kielce/tu/libraryApp/LibraryApp.java
@@ -0,0 +1,12 @@
+package pl.kielce.tu.libraryApp;
+
+/**
+ * @author ciepluchs
+ */
+public class LibraryApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
